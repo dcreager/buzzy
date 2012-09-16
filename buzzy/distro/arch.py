981f9a3e6e0257616a84007ad74159ac0ec2aee3
Douglas Creager <douglas.creager@redjack.com>
2012-09-16 17:49:16 -0400

    Support subdirectories in recipe database
    Each recipe's name is the relative path of its YAML file within the
    recipe database directory, without the ".yaml" extension.  The default
    package name is the basename of the recipe name.

--- buzzy/distro/arch.py
+++ buzzy/distro/arch.py
@@ -54,11 +54,13 @@ class Recipe(buzzy.recipe.Recipe):
         if isinstance(self.arch.native, str):
             package = NativePackage(self.arch.native, self)
         elif is_native_package(self.recipe_name):
-            package = NativePackage(self.recipe_name, self)
+            package_name = os.path.basename(self.recipe_name)
+            package = NativePackage(package_name, self)
         else:
             # For now, we only allow one built package per recipe.  That will
             # probably change.
-            package = BuiltPackage(self.recipe_name, self)
+            package_name = os.path.basename(self.recipe_name)
+            package = BuiltPackage(package_name, self)
 
         self.packages = [package]
 
