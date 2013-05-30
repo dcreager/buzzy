1d435bdd8c5caf885adafbf2b11b18e982fa06a3
Douglas Creager <douglas.creager@redjack.com>
2013-05-30 11:57:14 -0400

    Allow distros to override default global variables
    On 64-bit RPM distros, for instance, we need to update `lib_dir_name` to
    `lib64`, so that we install built shared libraries into the right
    location.

---
+++ src/libbuzzy/distro/env.c
@@ -0,0 +1,56 @@
+/* -*- coding: utf-8 -*-
+ * ----------------------------------------------------------------------
+ * Copyright © 2013, RedJack, LLC.
+ * All rights reserved.
+ *
+ * Please see the COPYING file in this distribution for license details.
+ * ----------------------------------------------------------------------
+ */
+
+#include <clogger.h>
+#include <libcork/core.h>
+#include <libcork/helpers/errors.h>
+
+#include "buzzy/env.h"
+#include "buzzy/distro.h"
+#include "buzzy/distro/posix.h"
+
+#include "buzzy/distro/arch.h"
+#include "buzzy/distro/rpm.h"
+
+#define CLOG_CHANNEL  "distro:env"
+
+
+static int
+bz_detect_arch(void)
+{
+    /* There aren't any variables to override on Arch Linux. */
+    return 0;
+}
+
+
+static int
+bz_detect_redhat(void)
+{
+    bool  is_redhat;
+    rii_check(bz_redhat_is_present(&is_redhat));
+    if (is_redhat) {
+        struct bz_env  *env = bz_global_env();
+        const char  *arch = bz_posix_current_architecture();
+        if (strcmp(arch, "x86_64") == 0) {
+            clog_info("Installing libraries into /usr/lib64");
+            bz_env_add_backup
+                (env, "lib_dir_name", bz_string_value_new("lib64"));
+        }
+    }
+    return 0;
+}
+
+
+int
+bz_distro_add_env_overrides(void)
+{
+    rii_check(bz_detect_arch());
+    rii_check(bz_detect_redhat());
+    return 0;
+}
