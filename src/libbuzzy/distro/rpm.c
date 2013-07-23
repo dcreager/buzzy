85602f108ba2460d97c9a919b6bfb7929df76d97
Douglas Creager <douglas.creager@redjack.com>
2013-07-23 17:49:15 -0400

    yum -C instead of --cacheonly
    The `--cacheonly` long option isn't available in RHEL5's yum version.
    The equivalent `-C` is available, however, in the older and newer
    versions.

--- src/libbuzzy/distro/rpm.c
+++ src/libbuzzy/distro/rpm.c
@@ -1045,7 +1045,7 @@ bz_yum_native_version_available(const char *native_package_name)
 
     rpi_check(bz_subprocess_get_output
               (&out, NULL, &successful,
-               "yum", "info", "--cacheonly", native_package_name, NULL));
+               "yum", "info", "-C", native_package_name, NULL));
     if (!successful) {
         cork_buffer_done(&out);
         return NULL;
