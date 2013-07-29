0680e370ec84d039d314a1fd7ecf23ec48b8a2e8
Douglas Creager <douglas.creager@redjack.com>
2013-07-29 10:11:10 -0400

    Bump Homebrew version for 0.4.1

--- spec/buzzy.rb
+++ spec/buzzy.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Buzzy < Formula
   homepage 'https://github.com/redjack/buzzy/'
-  version '0.4.0-dev'
+  version '0.4.1'
   url 'https://github.com/redjack/buzzy.git', :branch => 'master'
   head 'https://github.com/redjack/buzzy.git', :branch => 'develop'
 
